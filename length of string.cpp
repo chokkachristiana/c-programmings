8bae0f3241b4cfd7940e9c2e1e1116f60414fc74
chokkachristiana <chokkachristiana@gmail.com>
2025-11-16 17:15:31 +0530

    Add files via upload

---
+++ length of string.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char name[10];
+	int len=0,i;
+	puts("enter your number:");
+	gets(name);
+	for(i=0;name[i]!='\0';i++)
+	len++;
+	printf("length of your name is%d\n",len);
+	return 0;
+}
+
+	
+
