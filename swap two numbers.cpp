c3ec83f8f494cfe4d6d3df5f657b704398bb5052
chokkachristiana <chokkachristiana@gmail.com>
2025-11-16 16:06:59 +0530

    Add files via upload

---
+++ swap two numbers.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,temp;
+	printf("enter two numbers:");
+	scanf("%d%d",&a,&b);
+	printf("%d%d",&a,&b);
+	temp=a;
+	a=b;
+	b=temp;
+	printf("After swap:a=%d,b=%d",a,b);
+	return 0;
+}
