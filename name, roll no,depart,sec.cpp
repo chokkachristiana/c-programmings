a0dd512e0345587333bdf71a196f7d1dd8bd67b8
chokkachristiana <chokkachristiana@gmail.com>
2025-11-22 11:26:36 +0530

    Add files via upload

---
+++ name, roll no,depart,sec.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char name[10],rollnumber[10],department[10];
+	int sec;
+	puts("enter your name");
+	gets(name);
+	puts("enter your roll number");
+	gets(rollnumber);
+	puts("enter your department");
+	gets(department);
+	printf("enter your section");
+	scanf("%d",&sec);
+	return 0;
+}
