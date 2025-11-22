4dceceb751d8d0a928e592e42af9338fb74528d4
chokkachristiana <chokkachristiana@gmail.com>
2025-11-22 10:13:43 +0530

    Add files via upload

---
+++ minimum and maximum array.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main() 
+{
+int a[10],n,i,min,max;
+printf("enter number of elements:");
+scanf("%d",&n);
+printf("enter array elements:");
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+min=max=a[0];
+for(i=1;i<n;i++)
+{
+	if(max<a[i])
+	max=a[i];
+	}
+	if(min>a[i])
+	{
+		min=a[i];
+	}
+	printf("min=%d\n",min);
+	printf("max=%d\n",max);
+	return 0;
+}
